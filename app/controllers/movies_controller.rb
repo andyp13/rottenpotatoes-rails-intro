d7030ea3aaa5445566583720adc84eb646f53eab
Andy Phillips <andy@phillips101.com>
2017-09-25 19:23:18 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sorting_col = params[:sort_by]
+    @sorting = params[:sort_by]
+    @movies = Movie.order(@sorting).all
+    #redirect_to movies_path(:sort_by=>@sorting)
+    
+    
   end
 
   def new
